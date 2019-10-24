10be6894f1f7b2d87da2cbcf8c68321c1b4a28a2
Shadman Sakib <sakib.buet51@outlook.com>
2019-10-24 21:09:09 +0600

    Init Theme

---
+++ Server/Web.Infrastructure/Services/CourseService.cs
@@ -0,0 +1,52 @@
+﻿using CoreEngine.Model.DBModel;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Web.Infrastructure.DBModel;
+
+namespace Web.Infrastructure.Services
+{
+    public class CourseService
+    {
+        private readonly StudentDBContext _db;
+
+        public CourseService(StudentDBContext dBContext)
+        {
+            _db = dBContext;
+        }
+
+        public async Task<bool> AddCourse(Course course, int batchId)
+        {
+            var oldCourse = await _db.Courses
+                                     .FirstOrDefaultAsync(x => x.Id == course.Id &&
+                                                               x.Batch.Id == batchId);
+            if (oldCourse != null) return false;
+            else
+            {
+                var batch = await _db.Batches.FirstOrDefaultAsync(x => x.Id == batchId);
+                if (batch == null) return false;
+                else
+                {
+                    _db.Entry(course).State = EntityState.Added;
+                    course.Batch = batch;
+                    await _db.SaveChangesAsync();
+                    return true;
+                }
+            }
+        }
+
+        public async Task<bool> ModifyCourse(Course course)
+        {
+            var dbCourse = await _db.Courses.FindAsync(course);
+            if (dbCourse == null)
+            {
+                return false;
+            }
+            else
+            {
+                _db.Entry(dbCourse).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+                return true;
+            }
+        }
+    }
+}
--- Server/Web.Infrastructure/Services/UserService.cs
+++ Server/Web.Infrastructure/Services/UserService.cs
@@ -2,17 +2,19 @@
 using CoreEngine.Model.Common;
 using CoreEngine.Model.DBModel;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Web.Infrastructure.DBModel;
 
 namespace Web.Infrastructure.Services
 {
-    public class UserRepository
+    public class UserService
     {
         readonly UserManager<DBUser> _usermanager;
         readonly StudentDBContext _db;
-        public UserRepository(UserManager<DBUser> userManager, StudentDBContext studentDB)
+        public UserService(UserManager<DBUser> userManager, StudentDBContext studentDB)
         {
             _usermanager = userManager;
             _db = studentDB;
@@ -24,6 +26,9 @@ namespace Web.Infrastructure.Services
             var batch = await _db.Batches.FindAsync(batchID);
             foreach (var student in students)
             {
+                var oldUser = _db.Users.FirstOrDefaultAsync(x => x.UserName == student.UserName);
+                if (oldUser != null) continue;
+
                 var password = CryptoService.GenerateRandomPassword();
                 student.Batch = batch;
                 var res = await _usermanager.CreateAsync(student);
