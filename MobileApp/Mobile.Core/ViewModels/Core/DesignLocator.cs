c65b77b85c1483d230d4281268c12341ba6ccf92
Shadman Sakib <sakib.buet51@outlook.com>
2019-12-02 11:04:19 +0600

    sync

--- MobileApp/Mobile.Core/ViewModels/Core/DesignLocator.cs
+++ MobileApp/Mobile.Core/ViewModels/Core/DesignLocator.cs
@@ -7,19 +7,24 @@ namespace Mobile.Core.ViewModels
 {
     public static class DesignLocator
     {
-        public static LoginViewModel LoginViewModel => Locator.GetInstance<LoginViewModel>();
-        public static ForgetPassViewModel ForgetPassViewModel => Locator.GetInstance<ForgetPassViewModel>();
-        public static AddUpdateCourseViewModel AddUpdateCourseViewModel => Locator.GetInstance<AddUpdateCourseViewModel>();
-        public static AddUpdateLessonViewModel AddUpdateLessonViewModel => Locator.GetInstance<AddUpdateLessonViewModel>();
-        public static CourseDetailViewModel CourseDetailViewModel => Locator.GetInstance<CourseDetailViewModel>();
-        public static CoursesViewModel CoursesViewModel => Locator.GetInstance<CoursesViewModel>();
-        public static HomeViewModel HomeViewModel => Locator.GetInstance<HomeViewModel>();
-        public static AddUpdateNoticeViewModel AddUpdateNoticeViewModel => Locator.GetInstance<AddUpdateNoticeViewModel>();
-        public static NoticeDetailViewModel NoticeDetailViewModel => Locator.GetInstance<NoticeDetailViewModel>();
-        public static NoticesViewModel NoticesViewModel => Locator.GetInstance<NoticesViewModel>();
-        public static ProfileDetailViewModel ProfileDetailViewModel => Locator.GetInstance<ProfileDetailViewModel>();
-        public static ProfilesViewModel ProfilesViewModel => Locator.GetInstance<ProfilesViewModel>();
-        public static SplashViewModel SplashViewModel => Locator.GetInstance<SplashViewModel>();
-        public static MainViewModel MainViewModel => Locator.GetInstance<MainViewModel>();
+        public static LoginViewModel LoginViewModel => GetLocator<LoginViewModel>();
+        public static ForgetPassViewModel ForgetPassViewModel => GetLocator<ForgetPassViewModel>();
+        public static AddUpdateCourseViewModel AddUpdateCourseViewModel => GetLocator<AddUpdateCourseViewModel>();
+        public static AddUpdateLessonViewModel AddUpdateLessonViewModel => GetLocator<AddUpdateLessonViewModel>();
+        public static CourseDetailViewModel CourseDetailViewModel => GetLocator<CourseDetailViewModel>();
+        public static CoursesViewModel CoursesViewModel => GetLocator<CoursesViewModel>();
+        public static HomeViewModel HomeViewModel => GetLocator<HomeViewModel>();
+        public static AddUpdateNoticeViewModel AddUpdateNoticeViewModel => GetLocator<AddUpdateNoticeViewModel>();
+        public static NoticeDetailViewModel NoticeDetailViewModel => GetLocator<NoticeDetailViewModel>();
+        public static NoticesViewModel NoticesViewModel => GetLocator<NoticesViewModel>();
+        public static ProfileDetailViewModel ProfileDetailViewModel => GetLocator<ProfileDetailViewModel>();
+        public static ProfilesViewModel ProfilesViewModel => GetLocator<ProfilesViewModel>();
+        public static SplashViewModel SplashViewModel => GetLocator<SplashViewModel>();
+        public static MainViewModel MainViewModel => GetLocator<MainViewModel>();
+
+        private static T GetLocator<T>()
+        {
+            return default;
+        }
     }
 }
