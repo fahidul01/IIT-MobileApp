1b57156ad6b2484c70dba9362b64a5a0bb5becc0
Shadman Sakib <sakib.buet51@outlook.com>
2019-12-08 12:52:47 +0600

    push

--- MobileApp/MobileApp.Android/MainActivity.cs
+++ MobileApp/MobileApp.Android/MainActivity.cs
@@ -3,14 +3,17 @@ using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Java.Security;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Mobile.Core.Engines.Services;
 using MobileApp.Droid.Services;
+using System;
 using Xamarin.Forms;
 
 namespace MobileApp.Droid
 {
+    //yoKOOpnaexb8JyrClG6ts00MiYU=
     [Activity(Label = "MobileApp", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
@@ -27,8 +30,33 @@ namespace MobileApp.Droid
             FormsMaterial.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
 
+            //PrintId();
             LoadApplication(App.Init(ConfigureServices));
         }
+
+        private void PrintId()
+        {
+            try
+            {
+                PackageInfo info = Android.App.Application.Context.PackageManager.GetPackageInfo(Android.App.Application.Context.PackageName, PackageInfoFlags.Signatures);
+                foreach (var signature in info.Signatures)
+                {
+                    MessageDigest md = MessageDigest.GetInstance("SHA");
+                    md.Update(signature.ToByteArray());
+
+                    System.Diagnostics.Debug.WriteLine(Convert.ToBase64String(md.Digest()));
+                }
+            }
+            catch (NoSuchAlgorithmException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
